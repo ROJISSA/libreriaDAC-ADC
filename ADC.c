1251d74e8ece959c725b54575af45f1f63566548
ROJISSA <141443385+ROJISSA@users.noreply.github.com>
2023-09-15 15:07:23 -0300

    Update ADC.c

--- ADC.c
+++ ADC.c
@@ -1,22 +1,15 @@
 #include "MKL25Z4.h"
 #include "stdio.h"
-#include "Libreria_ADC.h"
-#include "Delay.h"
+#include "ADC.h"
 
-void ADCx_SC1n(){
+void AdcInit(){
     SIM->SCGC6 |= SIM_SCGC6_ADC0_MASK;
+    ADC0->CFG1 = ADC_CFG1_MODE(3) | ADC_CFG1_ADICLK(1) | ADC_CFG1_ADIV(0);
+    ADC0->SC3 = ADC_SC3_AVGE_MASK | ADC_SC3_AVGS(3);
+    while (ADC0->SC3 & ADC_SC3_CAL_MASK);
 }
-void ADCn->SC1[0]{
-    ADC0->CFG1 = ADC_CFG1_MODE(3);
-    ADC0->CFG1 = ADC_CFG1_ADICLK(1);
-    ADC0->CFG1 = ADC_CFG1_ADIV(0);
-}
-void SIM_SCGC6(){
-    ADC0->SC3 = ADC_SC3_AVG3_MASK;
-    ADC0->SC3 = ADC_SC3_AVGS(3);
-}
-void ADCx_Rn(){
-   while(ADC->SC3 & ADC_SC3_CAL_MASK);
-}
-void ADC->R[0]{
+int AdcConversion(int canal){
+    ADC0->SC1[0] = ADC_SC3_ADCH(canal);
+    while (!(ADC0->SC1[0] & ADC_SC1_COCO_MASK));
+    return ADC0->R[0];
 }
