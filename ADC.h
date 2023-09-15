26e5426adb339cd0f521a306e858363450777b5b
ROJISSA <141443385+ROJISSA@users.noreply.github.com>
2023-09-15 15:07:51 -0300

    Update ADC.h

--- ADC.h
+++ ADC.h
@@ -1,11 +1,9 @@
-#ifndef "Libreria_ADC.h"
+#ifndef ADC.H
+#define DAC_H
 
-void ADCx_SC1n();
-void ADCn->SC1[0];
-void SIM_SCGC6();
-void ADCx_Rn();
-void ADC->R[0];
-void ADCx_CFG1();
-void ADCx_SC3();
+#include <stdint.h>
+
+void AdcInit();
+int AdcConversion();
 
 #endif
