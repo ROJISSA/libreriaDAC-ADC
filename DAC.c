d51f54bbafaf25c4c02e11b4f2a0785814560a7f
ROJISSA <141443385+ROJISSA@users.noreply.github.com>
2023-09-15 15:08:12 -0300

    Update DAC.c

--- DAC.c
+++ DAC.c
@@ -1,17 +1,12 @@
 #include "MKL25Z4.h"
 #include "stdio.h"
-#include "Libreria_DAC.h"
+#include "DAC.h"
 
-void SIM_SCGC2(){
-    SIM->SCGC2 |= SIM_SCGC2_DAC0_MASK;
+void DacInit(){
+    SIM->SCGC6 |= SIM_SCGC6_DAC0_MASK;
+    DAC0->C0 = DAC_C0_DACEN_MASK | DAC_C0_DACRFS_MASK;
 }
-void DACx_C0(){
-    DAC0->C0 = DAC_C0_DACEN_MASK;
-    DAC0->C0 = DAC_C0_DACRFS_MASK;
-}
-void DACx_DATnL(){
-    DAC0->DAT[0].DATH = DAC_DATH_DATA1(valor>>8);
-}
-void DACx_DATnH(){
-    DAC0->DAT[0].DATL = DAC_DATL_DATA0(valor & 0xFF);
+void DacConversion(int valor0){
+    DAC0->DAT[0].DATH = DAC_DATH_DATA1(valor0 >> 8);
+    DAC0->DAT[0].DATH = DAC_DATH_DATA0(valor0 & 0xFF);
 }
